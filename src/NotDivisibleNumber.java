420b1923a656816ebe814cd4392bc824f46ecef8
Bozhidar <nikolchevbobi4@gmail.com>
2025-02-13 22:02:08 +0100

    Initial commit

---
+++ src/NotDivisibleNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NotDivisibleNumber {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int targetNumber = Integer.parseInt(scanner.nextLine());
+
+        int number = 1;
+
+        for (int i = 0; i < targetNumber; i++) {
+            if (number % 3 != 0 && number % 7 != 0) {
+                System.out.print(number + " ");
+            }
+
+            number++;
+        }
+    }
+}
