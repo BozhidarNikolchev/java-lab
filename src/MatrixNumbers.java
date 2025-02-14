7ddf3a8d7a84676d4e930e696feb073d90af3b85
Bozhidar <nikolchevbobi4@gmail.com>
2025-02-14 21:07:51 +0100

    Initial commit

---
+++ src/MatrixNumbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class MatrixNumbers {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(j + i + 1 + " ");
+            }
+
+            System.out.println();
+        }
+
+
+
+
+
+
+    }
+}
